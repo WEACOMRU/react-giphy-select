5297044541ca1e71eee394461cfdec090bcebbe4
bashkos <bashkos@gmail.com>
2019-03-18 01:24:46 +0900

    Build docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,20 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
 const reactHotLoader = require('react-hot-loader/babel');
 
 const docsDir = resolve(__dirname, 'docs');
+const docsDistDir = resolve(__dirname, 'docs-dist');
 
-module.exports = {
+const common = {
   context: docsDir,
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
     './index.js',
   ],
   output: {
     filename: 'bundle.js',
-    path: docsDir,
-    publicPath: '/',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    contentBase: docsDir,
-    publicPath: '/',
   },
   module: {
     rules: [
@@ -59,7 +52,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
   ],
   resolve: {
@@ -68,3 +60,44 @@ module.exports = {
     },
   },
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = merge(common, {
+    output: {
+      path: docsDistDir,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin(),
+      new CopyPlugin([
+        {
+          from: resolve(docsDir, 'index.html'),
+          to: resolve(docsDistDir, 'index.html')
+        }
+      ]),
+    ],
+  });
+} else {
+  module.exports = merge(common, {
+    entry: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+    ],
+    output: {
+      path: docsDir,
+      publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      hot: true,
+      contentBase: docsDir,
+      publicPath: '/',
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  });
+}
